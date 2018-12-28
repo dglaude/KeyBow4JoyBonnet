5e6bdbc81274b2e2ac8a6f54f088c4d80d654a14
David Glaude <david.glaude@gmail.com>
2018-12-28 22:33:58 +0000

    Update header

--- keybow/keybow.h
+++ keybow/keybow.h
@@ -4,10 +4,15 @@
 #include "lua-config.h"
 
 #define NUM_KEYS 12
+#define NUM_ANAL_KEYS 4
+
+#define ANALOG_THRESH_POS -600
+#define ANALOG_THRESH_NEG 600
 
 pthread_mutex_t lights_mutex;
 
 unsigned short last_state[NUM_KEYS];
+unsigned short analog_states[NUM_ANAL_KEYS];
 
 int lights_auto;
 
